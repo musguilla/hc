a573d28fa0ecfca314450aedc7e17ea215958e11
muyfresh <muyfreshbos@gmail.com>
2026-04-18 16:29:11 +0200

    style(bot): remove 'content' word from product titles for leaner UI layout

--- src/services/product.service.js
+++ src/services/product.service.js
@@ -13,32 +13,32 @@ const products = [
   },
   {
     code: 'exclusive_content_level_1',
-    title: 'Exclusive content L1',
-    description: 'Hashcoin Exclusive content L1',
+    title: 'Exclusive L1',
+    description: 'Hashcoin Exclusive L1',
     amount_xtr: 45373
   },
   {
     code: 'exclusive_content_level_2',
-    title: 'Exclusive content L2',
-    description: 'Hashcoin Exclusive content L2',
+    title: 'Exclusive L2',
+    description: 'Hashcoin Exclusive L2',
     amount_xtr: 90746
   },
   {
     code: 'exclusive_content_level_3',
-    title: 'Exclusive content L3',
-    description: 'Hashcoin Exclusive content L3',
+    title: 'Exclusive L3',
+    description: 'Hashcoin Exclusive L3',
     amount_xtr: 226865
   },
   {
     code: 'exclusive_content_level_4',
-    title: 'Exclusive content L4',
-    description: 'Hashcoin Exclusive content L4',
+    title: 'Exclusive L4',
+    description: 'Hashcoin Exclusive L4',
     amount_xtr: 453731
   },
   {
     code: 'exclusive_content_level_5',
-    title: 'Exclusive content L5',
-    description: 'Hashcoin Exclusive content L5',
+    title: 'Exclusive L5',
+    description: 'Hashcoin Exclusive L5',
     amount_xtr: 680596
   },
   {
@@ -49,20 +49,20 @@ const products = [
   },
   {
     code: 'vip_content_level_1',
-    title: 'VIP content L1',
-    description: 'Hashcoin VIP content L1',
+    title: 'VIP L1',
+    description: 'Hashcoin VIP L1',
     amount_xtr: 907462
   },
   {
     code: 'vip_content_level_2',
-    title: 'VIP content L2',
-    description: 'Hashcoin VIP content L2',
+    title: 'VIP L2',
+    description: 'Hashcoin VIP L2',
     amount_xtr: 1361192
   },
   {
     code: 'vip_content_level_3',
-    title: 'VIP content L3',
-    description: 'Hashcoin VIP content L3',
+    title: 'VIP L3',
+    description: 'Hashcoin VIP L3',
     amount_xtr: 2268654
   }
 ];
